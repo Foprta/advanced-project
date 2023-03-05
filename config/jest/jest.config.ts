9fcd492f6aa3bd443193fa7c730cbb0c2a56980a
Evgenii Klinnikov <mostransformer@gmail.com>
2023-03-05 13:26:07 +0400

    jest

---
+++ config/jest/jest.config.ts
@@ -0,0 +1,13 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+    clearMocks: true,
+    testEnvironment: 'jsdom',
+    moduleDirectories: ['node_modules'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+    rootDir: '../../',
+    testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
+};
